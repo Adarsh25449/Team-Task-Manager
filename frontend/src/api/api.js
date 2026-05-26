52bce0dd9ce2aa6bbd23bda693960215ca5205f1
Adarsh <adarshanand@2104@gmail.com>
2026-05-26 18:09:46 +0530

    Update API URL for Render deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-s9pd.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
+
+export default API;
 
 export default API;
